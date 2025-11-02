07c6cf26cc0a9dee869d324fdc73a8310b1e06d0
PuskarDaju <puskarniroula8@gmail.com>
2025-11-02 16:14:32 +0545

    changed the ui of book/index

--- Library Management System/Controllers/Admin/CategoryController.cs
+++ Library Management System/Controllers/Admin/CategoryController.cs
@@ -8,23 +8,27 @@ namespace Library_Management_System.Controllers.Admin;
 
 public class CategoryController(ICategoryService service):Controller
 {
+    
 
     private readonly ICategoryService _service=service ?? throw new ArgumentNullException(nameof(service));
    
 
     public async Task<IActionResult> Index()
     {
+        ViewBag.ActiveMenu="Category";
         var category = await _service.GetAllCategoriesAsync();
         return View($"~/Views/Admin/Category/Index.cshtml",category);
     }
 
     public IActionResult Create()
     {
+        ViewBag.ActiveMenu="Category";
         return View("~/Views/Admin/Category/Create.cshtml");
     }
     
     public async Task<IActionResult> Edit(int id)
     {
+        ViewBag.ActiveMenu="Category";
         var category=await _service.GetCategoryByIdAsync(id);
         if (category==null) return NotFound(
         new
